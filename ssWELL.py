490918cdb28dbb6f10c70e77b0f03be46261c911
Rpon420 <pronabkumarrana@gmail.com>
2026-07-06 16:56:10 +0600

    Implement even or odd number checker
    Add a script to check if numbers are even or odd.

---
+++ ssWELL.py
@@ -0,0 +1,10 @@
+e= input("enter a number ")
+while e!='':
+    f=int(e)
+    y=f%2 
+    if y==0 :
+        print (f"yes {f} is a even")
+    else :
+        print (f"yes {f} is an odd")
+    e= input("enter a number ")
+g=input ("press any other to get out")
